d9417338011664982ab216ef3d7b3438509776b8
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2017-04-15 20:18:56 +0530

    Initial Commit

---
+++ mobile/src/main/java/com/example/sarvex/iscream/MyApplication.java
@@ -0,0 +1,31 @@
+package com.example.sarvex.iscream;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+
+/**
+ * Created by sarvex on 15/4/17.
+ */
+
+public class MyApplication extends Application {
+
+  private RefWatcher refWatcher;
+
+  public static RefWatcher getRefWatcher(Context context) {
+    MyApplication application = (MyApplication) context.getApplicationContext();
+    return application.refWatcher;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    if (LeakCanary.isInAnalyzerProcess(this)) {
+      return;
+    }
+
+    refWatcher = LeakCanary.install(this);
+  }
+}
